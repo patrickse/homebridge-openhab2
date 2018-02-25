c5e46746edc337eb9b2530cd976aae3370c97497
Patrick Sernetz <patrick@sernetz.com>
2018-02-25 07:26:28 +0100

    Adding update handler for temperature sensor and handle negative values

--- src/accessories/temperatureSensor.ts
+++ src/accessories/temperatureSensor.ts
@@ -1,6 +1,10 @@
 'use strict';
 
 import { AbstractAccessory } from './abstracts/abstractAccessory';
+import { OpenHAB2DeviceInterface } from '../models/platform/openHAB2DeviceInterface';
+import { Deferred } from 'ts-deferred';
+
+
 //import { platform } from 'os';
 
 export class TemperatureSensor extends AbstractAccessory {
@@ -10,9 +14,15 @@ export class TemperatureSensor extends AbstractAccessory {
         this.otherService = this.getService(this.hapService.TemperatureSensor, this.displayName);
 
         this.getCharacteristic(this.hapCharacteristic.CurrentTemperature, this.otherService)
-        //.on('set', this.setItemPowerState.bind(this))
-        //.on('get', this.getItemPowerState.bind(this))
-            .setValue(+this.state, () => { }, 'init');
+            .setProps({
+            format: this.hapCharacteristic.Formats.FLOAT,
+            unit: this.hapCharacteristic.Units.CELSIUS,
+            maxValue: 100,
+            minValue: -100,
+            minStep: 0.1,
+            perms: [this.hapCharacteristic.Perms.READ, this.hapCharacteristic.Perms.NOTIFY]
+            })
+            .setValue(Number(this.state), () => { }, 'init');
 
     };
 
@@ -26,6 +36,52 @@ export class TemperatureSensor extends AbstractAccessory {
         
         return result 
 
+    };
+
+    getCurrentTemperature(callback) {
+        this.platform.log(`iOS - request power state from <${this.name}>`);
+    
+        this.getItemState()
+          .then(state => callback(null, Number(state)));
+      };
+    
+
+    getItemState() {
+        return this.platform.openHAB2Client.getDeviceProperties(this.name)
+          .then((device: OpenHAB2DeviceInterface) => {
+            this.platform.log(`OpenHAB2 HTTP - response from <${this.name}>: ${device.state}`);
+            return device.state;
+          })
+          .catch((err) => {
+            this.platform.log(`OpenHAB2 HTTP - error from <${this.name}>:`, err);
+            return Promise.reject(err);
+          });
+      };
+
+      updateCharacteristics(message: string) {
+
+        let characteristicTemperatureDeferred: Deferred<string> = new Deferred<string>();
+        let characteristicOnDeferred: Deferred<string> = new Deferred<string>();
+        let characteristicsUpdated : [Promise<string>,Promise<string>] = [characteristicTemperatureDeferred.promise, characteristicOnDeferred.promise];
+  
+        this.platform.log(`OpenHAB2 SSE - message from <${this.name}>: ${message}`);
+  
+        const temperature = Number(message);
+  
+        this.getCharacteristic(this.hapCharacteristic.CurrentTemperature, this.otherService)
+          .setValue(temperature, () => {
+            this.state = message;
+            characteristicTemperatureDeferred.resolve(message);
+          }, 'remote');
+  
+          /**
+        this.getCharacteristic(this.hapCharacteristic.On, this.otherService)
+          .setValue(brightness > 0, () => {
+            this.state = message;
+            characteristicOnDeferred.resolve(message);
+          }, 'remote');
+   */
+        return Promise.all<string, string>(characteristicsUpdated);
     };
 
 }
